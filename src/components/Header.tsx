1da78697d444361fb96ea7b73483f61164ecee38
Nalla Venkat <venkatnalla000@gmail.com>
2026-03-08 10:23:06 +0530

    rtyui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -122,7 +122,7 @@ const Header = () => {
             {/* Logo */}
             <Link 
               to="/" 
-              className="text-xl font-bold text-black"
+              className="text-lg font-bold text-black whitespace-nowrap"
             >
               Work Wizards Innovations
             </Link>
